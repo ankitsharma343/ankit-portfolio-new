c591d02138c93650511757d1f5c4c49fc70bcf6c
ankitsharma343 <ankitsharma081233@gmail.com>
2026-08-07 15:33:10 +0530

    update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,11 @@ import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer"; 
-import Seo from "./components/Seo";
 
 
 function App() {
   return (
     <div className="bg-black text-white overflow-x-hidden">
-      <Seo /> 
       <Navbar />
       <Hero />
       <About />
